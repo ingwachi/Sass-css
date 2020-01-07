3e201468cff1655bfabc14aff6d7d0c2e8014664
Wachirawit Onchet <ingwachi@gmail.com>
2020-01-07 12:15:22 +0700

    JQuery

--- public/js/myjquery.js
+++ public/js/myjquery.js
@@ -10,11 +10,26 @@ $(document).ready(function () {
         // $('h1.display-1').slideDown()
 
         var h1 = $('h1.display-1')
-        h1.slideUp()
-        h1.slideDown()
+        // h1.slideUp()
+        // h1.slideDown()
+        // h1.text("Hello World")
     }, 500);
 
     $('button.clickme').on('click', function() {
-        
+        var text = $('h1.display-1').text();
+        // $('h2.display-2').text(text);
+        $('h2.display-2').html(`<ul><li>${text}</li></ul>`)
+        console.log('Button was clicked');
     })
+
+    setTimeout(function() {
+        $.ajax({
+            url: 'data.json'
+        }).done(function (res) {
+            // console.log(res)
+            for (obj of res) {
+                console.log(obj.name + ": " + obj.color);
+            }
+        })
+    }, 2000)
 })
